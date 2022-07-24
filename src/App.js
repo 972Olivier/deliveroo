902f69fdffeaf8502e0d634f1075db301714d2a7
972Olivier <oliviercen@gmail.com>
2022-07-24 18:23:41 +0200

    change for heroku

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3100/"); // à changer quand cela sera sur heroku
+        const response = await axios.get(
+          "https://backend-deliverooo.herokuapp.com/"
+        ); // à changer quand cela sera sur heroku
         // console.log(response.data);
         setData(response.data);
         setIsLoading(false);
